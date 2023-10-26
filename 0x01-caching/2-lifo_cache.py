d473c8f68b95cc42bec24ba68861d0131faad805
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-26 17:45:34 +0100

    task 4

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -9,17 +9,18 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         '''Initiliaze'''
         super().__init__()
+        self.key_index = []
 
     def put(self, key, item):
         '''assign to the dictionary self.cache_data
         the item value for the key key
         '''
         if key and item:
+            if key in self.cache_data.keys():
+                self.cache_data.pop(key)
             if len(self.cache_data) >= self.MAX_ITEMS:
                 rm = self.cache_data.popitem()
                 print('DISCARD: {}'.format(rm[0]))
-            if key in self.cache_data.keys():
-                self.cache_data.pop(key)
             self.cache_data[key] = item
 
     def get(self, key):
